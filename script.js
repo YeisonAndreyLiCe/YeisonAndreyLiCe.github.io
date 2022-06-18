11ba319116bf22c97a46a836c55081c534720765
Yeison Andrey Liscano Ceballes <yliscanoc@unal.edu.co>
2022-06-18 00:46:22 -0500

    adding script

---
+++ script.js
@@ -0,0 +1,39 @@
+
+function changeName() {
+    var name = document.querySelector("#name");
+    name.innerText = "New Name";
+}
+
+
+var number = document.querySelector(".request_number");
+var count = number.value; 
+var connections = document.querySelector(".connections_number");
+var connectionsCount = 500;
+
+function remove1(element) {
+    var contact1 = document.querySelector(".usercontact1");
+    contact1.remove()
+    count--;
+    number.value = count;
+    if(element.classList.includes("check")) {
+        connectionsCount++;
+        connections.innerText = connectionsCount + "+";
+    } else {
+        connectionsCount--;
+        connections.innerText = connectionsCount + "+";
+    }
+}
+function remove2(element) {
+    var contact2 = document.querySelector(".usercontact2");
+    contact2.remove()
+    count--;
+    number.value = count;
+    console.log(element.classList)
+    if(element.classList.includes("check")){
+        connectionsCount++;
+        connections.innerText = connectionsCount + "+";
+    } else {
+        connectionsCount--;
+        connections.innerText = connectionsCount + "+";
+    }
+}
